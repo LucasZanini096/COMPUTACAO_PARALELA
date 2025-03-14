2aa3e2fc96dc866a427ed952b0961e4ff81842dc
LucasZanini096 <lucas608@outlook.com>
2025-03-14 08:03:23 -0300

    feat: first commit

---
+++ Lab04/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdint.h>
+
+int main(void) {
+   float saldo = 1000.0;
+
+   // depositos ()
+   // saques ()
+
+   return 0;
+}
